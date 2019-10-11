c06913c6403e87bdd238c1ab04c5fda80a7a45c3
Łukasz Kania <waniaskfr@gmail.com>
2019-10-11 16:40:10 +0200

    Change script.js Add class 'active'

--- js/script.js
+++ js/script.js
@@ -3,21 +3,26 @@
     console.log('links:', links);
   });*/
 
-  const titleClickHandler = function(){
+  const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
+    
 
-
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
 
-  /* remove class 'active' from all articles */
+  console.log('clickedElement:', clickedElement);
+
+
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts .active');
 
   for(let activeArticle of activeArticles){
