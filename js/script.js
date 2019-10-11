6fdb8f82c5f04b46bb40928e506735f6e24f904c
Łukasz Kania <waniaskfr@gmail.com>
2019-10-11 19:56:44 +0200

    Change script.js combine and display articles with links

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
   });*/
 
   const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
@@ -14,25 +15,30 @@
 
   for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
-}
+  }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
   clickedElement.classList.add('active');
 
   console.log('clickedElement:', clickedElement);
-
 
   /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts .active');
 
   for(let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
-}
-  /* get 'href' attribute from the clicked link */
+  }
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
+  
+   /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
 
-  /* add class 'active' to the correct article */
+  /* [DONE] add class 'active' to the correct article */
+  targetArticle.classList.add('active');
 }
   
   const links = document.querySelectorAll('.titles a');
