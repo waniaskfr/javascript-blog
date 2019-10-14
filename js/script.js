7427d68f455678158b1455a527f63e284e921037
Łukasz Kania <waniaskfr@gmail.com>
2019-10-14 15:38:08 +0200

    change script.js add function generateTitleLinks

--- js/script.js
+++ js/script.js
@@ -33,16 +33,61 @@
   const articleSelector = clickedElement.getAttribute('href');
   console.log(articleSelector);
   
-   /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
   const targetArticle = document.querySelector(articleSelector);
   console.log(targetArticle);
 
   /* [DONE] add class 'active' to the correct article */
   targetArticle.classList.add('active');
 }
+    
+  const optArticleSelector = '.post',
+      optTitleSelector = '.post-title',
+      optTitleListSelector = '.titles';
+
+  function generateTitleLinks (){
+
+  /* [DONE] remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    
+    titleList.innerHTML = '';   
+  
+  /* [DONE] for each article */
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    let html = '';
+    
+    for(let article of articles){
+      console.log(article);
+      
+  /* [DONE] get the article id */
+    const articleId = article.getAttribute('id');
+    console.log(articleId);
+    
+  /* find the title element */
+  const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+  
+  /* get the title from the title element */
+  /* create HTML of the link */
+  const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+  console.log(linkHTML);
+
+  /* insert link into titleList */
+  titleList.innerHTML = titleList.innerHTML + linkHTML;
+  
+  /* insert link into html variable */
+  html = html + linkHTML;
+  console.log(html);
+  }
+  
+  titleList.innerHTML = html;
   
   const links = document.querySelectorAll('.titles a');
   
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+}
+
+  generateTitleLinks();
+  
